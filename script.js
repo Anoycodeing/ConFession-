38dbfc1a011f9dc7d74d8db3cf22f370c18e16b8
Anoycodeing <151125347+Anoycodeing@users.noreply.github.com>
2025-06-09 00:51:48 +0800

    script.js

--- script.js
+++ script.js
@@ -2,20 +2,26 @@ const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
+const wrapper = document.querySelector(".wrapper");
+const btnGroup = document.querySelector(".btn-group");
 
 yesBtn.addEventListener("click", () => {
     question.innerHTML = "Laahh..Really?! I lab u na poo!!";
     gif.src = "https://media1.tenor.com/m/JyqIc_WLYMIAAAAC/love-chubby.gif";
+    btnGroup.style.display = "none";
 });
 
 noBtn.addEventListener("click", () => {
+    const wrapperRect = wrapper.getBoundingClientRect();
     const noBtnRect = noBtn.getBoundingClientRect();
-    const maxX = window.innerWidth - noBtnRect.width;
-    const maxY = window.innerHeight - noBtnRect.height;
+    
+    const maxX = wrapperRect.width - noBtnRect.width;
+    const maxY = wrapperRect.height - noBtnRect.height;
 
     const randomX = Math.random() * maxX;
     const randomY = Math.random() * maxY;
 
+    noBtn.style.position = 'absolute';
     noBtn.style.left = `${randomX}px`;
     noBtn.style.top = `${randomY}px`;
 });
